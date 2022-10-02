d757d1159aa974212e0daee43e7731a17eb00277
Poshdad <Tanner.schmidt93@gmail.com>
2022-10-02 11:27:27 -0700

    Site is up and running

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221002.131052';
+const cacheName = 'chirpy-20221002.131523';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
